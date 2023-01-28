35e2d06ad048b90fc4e4eb5bd9095d62dfbb7e8e
gtokmak <gokhantkmk@gmail.com>
2023-01-28 11:04:59 +0300

    checkout and order added

--- src/app/model/rest.service.ts
+++ src/app/model/rest.service.ts
@@ -3,22 +3,23 @@ import { Injectable } from '@angular/core';
 import { Product } from './product.model';
 import { Observable, retry } from 'rxjs';
 import { Category } from './category.model';
+import { Order } from './order.model';
 
 @Injectable()
-
 export class RestService {
+  baseUrl: string = 'http://localhost:3500/';
 
-  baseUrl:string = "http://localhost:3500/";
+  constructor(private http: HttpClient) {}
 
-  constructor(private http:HttpClient) {
-   }
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl + 'products');
+  }
 
-   getProducts(): Observable<Product[]> {
-     return this.http.get<Product[]>(this.baseUrl + 'products');
-   }
+  getCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.baseUrl + 'categories');
+  }
 
-   getCategories(): Observable<Category[]> {
-     return this.http.get<Category[]>(this.baseUrl + 'categories');
-   }
-
+  saveOrder(order: Order): Observable<Order> {
+    return this.http.post<Order>(this.baseUrl + 'orders', order);
+  }
 }
